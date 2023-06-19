9d1df4284f0c062fda68bc0091be408880705eee
Maartala <Steffi.Gerstenmaier@t-online.de>
2023-06-19 15:26:35 +0200

    Signup und Login erfolgreich

--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import {User} from "./model/index.js";
 
+
 dotenv.config({ path: new URL("../.env", import.meta.url).pathname });
 
 const PORT = process.env.BE_PORT || 3000;
 const app = express();
+
+app.use(express.json()); // Parse request body as JSON
 
 const ReactAppDistPath = new URL("../front-end/dist/", import.meta.url);
 const ReactAppIndex = new URL("../front-end/dist/index.html", import.meta.url);
@@ -23,63 +26,52 @@ app.get("/api/status", (req, res) => {
   res.send({ status: "Ok" });
 });
 
-app.get("/api/signup", (req, res) => {
-  res.send({ status: "Ok" });
-});
 
+app.post("/api/signup", async (req,res)=>{
+  // res.send({status: "Signup funktioniert"})
+  const{name, email, password}=req.body;
 
-
-
-app.post("/api/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    // Überprüft, ob User existiert
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: "User already exists" });
+  try{
+    
+    const existingUser=await User.findOne({email});
+    if (existingUser){
+      return res.status(409).json({error: "Benutzer mit dieser E-Mail existiert bereits "});
     }
-
-    // Erstelle einen neuen Benutzer
-    const newUser = new User({ name, email });
+    
+    const newUser =new User({
+      name,
+      email,
+    });
     newUser.setPassword(password);
-
-    // Speichert User in DB
     await newUser.save();
+    res.status(201).json({message: "Registrierung erfolgreich."});
 
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ message: "Internal server error" });
+  }catch(error){
+    console.log(error);
+    res.status(500).json({error: "Interner Serverfehler"})
   }
 });
 
+app.post("/api/login", async (req,res)=>{
+const {email, password}=req.body;
 
+try{
+  const existingUser = await User.findOne({email});
+  if(!existingUser){
+    return res.status(401).json({message: "Ungültige E-Mail oder Passwort"})
+  }
 
+  const isPasswordValid=existingUser.verifyPassword(password);
+  if(!isPasswordValid){
+    return res.status(401).json({message: "Ungültige E-Mail oder Passwort"})
+  }
 
-// app.post("/api/signup", async (req,res)=>{
-//   try{
-//     const{name, email, password}=req.body;
-
-//     const existingUser=await User.findOne({email});
-//     if (existingUser){
-//       return res.status(409).json({error: "Benutzer mit dieser E-Mail existiert bereits "});
-//     }
-
-//     const newUser =new User({
-//       name,
-//       email,
-//     });
-
-//     newUser.setPassword(password);
-
-//     await newUser.save();
-
-//     res.status(201).json({message: "Registrierung erfolgreich."});
-//   }catch(error){
-//     res.status(500).json({error: "Interner Serverfehler"})
-//   }
-// });
+  res.status(200).json({message: "Login erfolgreich"});
+}catch (error){
+  console.log("Fehler beim Einloggen: ", error);
+  res.status(500).json({message:"Internal error"})
+}
+})
 
 
 app.get("/*", (req, res) => {
@@ -90,3 +82,36 @@ app.get("/*", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server running on Port: ", PORT);
 });
+
+
+
+
+
+// app.get("/api/signup", (req, res) => {
+//   res.send({ status: "Ok" });
+// });
+
+
+// app.post("/api/signup", async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     // Überprüft, ob User existiert
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.status(409).json({ message: "User already exists" });
+//     }
+
+//     // Erstelle einen neuen Benutzer
+//     const newUser = new User({ name, email });
+//     newUser.setPassword(password);
+
+//     // Speichert User in DB
+//     await newUser.save();
+
+//     res.status(201).json({ message: "User created successfully" });
+//   } catch (error) {
+//     console.error("Error creating user:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
